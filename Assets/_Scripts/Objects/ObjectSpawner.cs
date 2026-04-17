0fbbdc5b5ba4a1dcfee5d76da85c0616c5055f7f
JoshuaHuntJames <S298011@uos.ac.uk>
2026-04-17 11:39:07 +0100

    Spawner Working

--- Assets/_Scripts/Objects/ObjectSpawner.cs
+++ Assets/_Scripts/Objects/ObjectSpawner.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 using Random = UnityEngine.Random;
 
 public class ObjectSpawner : MonoBehaviour
@@ -11,18 +13,21 @@ public class ObjectSpawner : MonoBehaviour
     public GameObject[] objectsToOutstantiate;
     public List<GameObject> spawnedObjectsList = new List<GameObject>();
     public float MoveForce;
+    private bool AllowObjSpawn;
+
+    public float spawnDelay = 1.5f;
 
     private bool firstObjSpawned;
 
-
     Rigidbody2D rb2D;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpawnObject();
-        firstObjSpawned = true;
+
+        AllowObjSpawn = true;
+        StartCoroutine(SpawnObject());
+
+
 
     }
 
@@ -32,14 +37,48 @@ public class ObjectSpawner : MonoBehaviour
 
     }
 
-    void SpawnObject()
+    IEnumerator SpawnObject()
     {
+        while (AllowObjSpawn)
+        {
             int n = Random.Range(0, objectsToInstatiate.Length);
+
             GameObject g = Instantiate(objectsToInstatiate[n], pos.position, objectsToInstatiate[n].transform.rotation);
             Rigidbody2D rb = g.GetComponent<Rigidbody2D>();
-
             rb.linearVelocity = transform.right * MoveForce;
 
             spawnedObjectsList.Add(g);
+
+            yield return new WaitForSeconds(spawnDelay);
+        }
     }
+
 }
+
+
+//public IEnumerator WaitUntilTrue(bool AllowObjSpawn)
+//{
+//    while (AllowObjSpawn == false)
+//    {
+//        yield return null;
+//    }
+//}
+
+//void SpawnObject()
+//{
+//    for (int i = 0; i < objectsToInstatiate.Length; i++)
+//    {
+//        if (AllowObjSpawn == true)
+//        {
+//            yield return WaitUntilTrue(AllowObjSpawn);
+//            int n = Random.Range(0, objectsToInstatiate.Length);
+//            GameObject g = Instantiate(objectsToInstatiate[n], pos.position, objectsToInstatiate[n].transform.rotation);
+//            Rigidbody2D rb = g.GetComponent<Rigidbody2D>();
+
+//            rb.linearVelocity = transform.right * MoveForce;
+
+//            spawnedObjectsList.Add(g);
+//        }
+//    }
+//}
+
