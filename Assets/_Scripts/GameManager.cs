779cd4a18693379a25ba1eeb2c176203cf43c623
NikolaosComandariu <s283743@uos.ac.uk>
2026-04-20 16:34:37 +0100

    Feat (GameManager): Set up game manager script

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Round Related")]
+    [SerializeField] private int currentRoundNumber;
+    
+    [Header("Timers")]
+    [SerializeField] private float startCountdown;
+    [SerializeField] private float roundCountdown;
+
+    [Header("Game Objects")]
+    [SerializeField] private GameObject startCountdownTimer;
+    [SerializeField] private GameObject roundTimer;
+
+    private int maxRoundNumber = 16;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startCountdown = 3.0f;
+        StartCoroutine(StartGameCountdown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator StartGameCountdown()
+    {
+        yield return null;
+    }
+
+    private void StartRoundCountdown()
+    {
+
+    }
+
+    private void IncreaseDifficulty()
+    {
+
+    }
+}
