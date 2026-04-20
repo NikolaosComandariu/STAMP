4442118dd7f89fd00716e3f4f0117f1ce6f37efd
JoshuaHuntJames <S298011@uos.ac.uk>
2026-04-20 11:28:54 +0100

    To be committed to main
    Objects scroll onto screen and can be "accepted" and "declined" using q and e respectfully.

--- Assets/_Scripts/Objects/ObjectSpawner.cs
+++ Assets/_Scripts/Objects/ObjectSpawner.cs
@@ -4,20 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField] private float spawnDelay = 1.5f;
     [SerializeField] private float MoveForce;
-    [SerializeField] private List<GameObject> ObjectsPool = new List<GameObject>();
-    [SerializeField] private Transform EndOfConveyor;
-    [SerializeField] private Transform SpawnPos;
+    [SerializeField] private List<GameObject> ObjectsPool = new List<GameObject>(); // Amount of objects in the round
+    [SerializeField] private Transform EndOfConveyor; // Stopping point of objects where they're ready to be accepted / declined
+    [SerializeField] private Transform SpawnPos; // Off screen spawnpoint for objects to then scroll onto screen
 
     private Button Accept;
     private Button Decline;
 
-    private int NumOfObjToSpawn;
+    private int NumOfObjToSpawn; // tally of items left to spawn
     private bool AllowObjSpawn;
+
+    private GameObject currentObject;
 
 
 
@@ -47,34 +50,47 @@ public class ObjectSpawner : MonoBehaviour
             {
                 int n = Random.Range(0, ObjectsPool.Count);
 
-                GameObject g = Instantiate(ObjectsPool[n], SpawnPos.position, ObjectsPool[n].transform.rotation);
+                currentObject = Instantiate(ObjectsPool[n], SpawnPos.position, ObjectsPool[n].transform.rotation);
                 ObjectsPool.RemoveAt(n);
                 NumOfObjToSpawn--;
 
-                MoveToTarget mover = g.GetComponent<MoveToTarget>();
-                mover.target= EndOfConveyor;
-                mover.speed =MoveForce;
+                MoveToTarget mover = currentObject.GetComponent<MoveToTarget>();
+                mover.target = EndOfConveyor;
+                mover.speed = MoveForce;
 
-                Rigidbody2D rb = g.GetComponent<Rigidbody2D>();
+
+
+                Rigidbody2D rb = currentObject.GetComponent<Rigidbody2D>();
                 //rb.linearVelocity = transform.right * MoveForce;
                 rb.transform.position = Vector2.MoveTowards(SpawnPos.position, EndOfConveyor.position, MoveForce * Time.deltaTime);
 
-                yield return new WaitForSeconds(spawnDelay);
+                AllowObjSpawn = false;
+
+                //yield return new WaitForSeconds(spawnDelay);
+                //AllowObjSpawn = false;
+
+                yield return null;
+
             }
         }
     }
 
-
-    //spawn one item then once button pressed then move item and only then spawn the next one and repeat
-
-    // on click of either button item is then destroyed, when first object is instantiated set a variable to signify an obj is present and the destroy on button press then set variable back to allow next obj to spawn
-
-
-    void AcceptOrDeclinePressed()
+    public void AcceptObject()
     {
-        if (Accept == true)
+        if (currentObject != null)
         {
-
+            Destroy(currentObject);
+            AllowObjSpawn = true;
+            StartCoroutine(SpawnObject());
+        }
+    }
+    public void DeclineObject()
+    {
+        if (currentObject != null)
+        {
+            Destroy(currentObject);
+            AllowObjSpawn = true;
+            StartCoroutine(SpawnObject());
         }
     }
 }
