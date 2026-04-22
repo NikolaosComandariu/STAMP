27e822c27c80511dc70226a40bf4601d81ab88a1
smrizz0 <s305703@uos.ac.uk>
2026-04-22 10:58:18 +0100

    completed the visual movement
    i did it for both the accepted and declined options :)

--- Assets/_Scripts/Objects/ObjectSpawner.cs
+++ Assets/_Scripts/Objects/ObjectSpawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 using UnityEngine.UI;
 using Unity.VisualScripting;
+using System.Threading;
+using System.Threading.Tasks;
 
 public class ObjectSpawner : MonoBehaviour
 {
@@ -77,12 +79,28 @@ public class ObjectSpawner : MonoBehaviour
 
     //testing stuff
     [SerializeField] private Transform DeclinedP1;
+    [SerializeField] private Transform AcceptedP1;
+    //public DeclinedTrigger trigger;
+    //private Collision2D collision;
+
+   /* public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("DeclinedTrigger"))
+        {
+            Debug.Log("Collision");
+            //currentObject.SetActive(false);
+        }
+    }*/
 
     public void AcceptObject()
     {
         if (currentObject != null)
         {
-            Destroy(currentObject);
+            MoveToTarget acceptedP1 = currentObject.GetComponent<MoveToTarget>();
+            acceptedP1.target = AcceptedP1;
+            acceptedP1.speed = MoveForce;
+            currentObject.transform.position = Vector2.MoveTowards(EndOfConveyor.position, AcceptedP1.position, MoveForce * Time.deltaTime);
+            //Destroy(currentObject);
             AllowObjSpawn = true;
             StartCoroutine(SpawnObject());
         }
@@ -92,10 +110,28 @@ public class ObjectSpawner : MonoBehaviour
         if (currentObject != null)
         {
             //edited testing
-            Rigidbody2D testrb = currentObject.GetComponent<Rigidbody2D>();
-            testrb.transform.position = Vector3.MoveTowards(EndOfConveyor.position,DeclinedP1.position, MoveForce * Time.deltaTime);
-           // Destroy(currentObject);
+            MoveToTarget declinedP1 = currentObject.GetComponent<MoveToTarget>();
+            declinedP1.target = DeclinedP1;
+            declinedP1.speed = MoveForce;
+           // Rigidbody2D testrb = currentObject.GetComponent<Rigidbody2D>();
+            //testrb.transform.position = Vector2.MoveTowards(EndOfConveyor.position,DeclinedP1.position, MoveForce * Time.deltaTime);
+            currentObject.transform.position = Vector2.MoveTowards(EndOfConveyor.position, DeclinedP1.position, MoveForce * Time.deltaTime);
+            //OnCollisionEnter2D(collision);
+            //trigger.
+            //GameObject oldObject = currentObject;
+            //Thread.Sleep(5000);
+            //currentObject.SetActive(false);
+
+            /* if (currentObject.transform.position == DeclinedP1.position)
+             {
+                 currentObject.SetActive(false);
+             }*/
+
+            
+
+            //Destroy(currentObject);
             AllowObjSpawn = true;
+            //Thread.Sleep(2000);
             StartCoroutine(SpawnObject());
         }
     }
