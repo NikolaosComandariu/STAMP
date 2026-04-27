995ac5d8205aeb3bc64b477bf77dd0f0cbafa756
JoshuaHuntJames <S298011@uos.ac.uk>
2026-04-27 11:51:11 +0100

    AudioManager
    Audio Manager - Master, Music and SFX audio channels. Sliders for music and SFX working.

--- Assets/_Scripts/Objects/AudioManager.cs
+++ Assets/_Scripts/Objects/AudioManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     [Header ("------ Audio Source -----")]
 =======
     [Header("------ Audio Source -----")]
@@ -20,12 +21,28 @@ public class AudioManager : MonoBehaviour
 
     }
 
+=======
+    [Header("-----Audio Source-----")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("-----Audio Clip-----")]
+    public AudioClip ItemAccepted;
+    public AudioClip ItemDeclined;
+
+    // Example of how to play sound effect -
+    //audioManager.PlaySFX(audioManager.(nameofsound));
+>>>>>>> Stashed changes
     public void PlaySFX(AudioClip clip)
     {
         SFXSource.PlayOneShot(clip);
     }
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
 =======
 }
 >>>>>>> Stashed changes
