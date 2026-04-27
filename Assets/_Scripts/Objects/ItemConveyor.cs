e2a8c19fe56f726fb82067913304f884b5016e16
Nikolaos Comandariu <s283743@uos.ac.uk>
2026-04-27 08:51:49 +0100

    Feat (Refactor): Refactor ItemConveyor.cs

--- Assets/_Scripts/Objects/ItemConveyor.cs
+++ Assets/_Scripts/Objects/ItemConveyor.cs
@@ -2,16 +2,14 @@ using UnityEngine;
 
 public class ItemConveyor : MonoBehaviour
 {
-    public Transform target;
-    public float speed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Position")]
+    [SerializeField] private Transform target; // Target position of item.
+
+    [Header("Speed")]
+    [SerializeField] private float speed; // Travel speed of item.
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
