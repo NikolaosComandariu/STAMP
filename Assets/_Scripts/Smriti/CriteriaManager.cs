e8ca22785170485763fbed08bed59d78d82db2bd
smrizz0 <s305703@uos.ac.uk>
2026-05-06 11:11:30 +0100

    Criteria Manager fully reworked
    price conditions still need to be sorted

--- Assets/_Scripts/Smriti/CriteriaManager.cs
+++ Assets/_Scripts/Smriti/CriteriaManager.cs
@@ -19,13 +19,15 @@ public class CriteriaManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _criteriaP2_3;
     // [SerializeField] private TextMeshProUGUI _criteria4;
 
-    private Dictionary<int, RoundCondition> CriteriaList = new Dictionary<int, RoundCondition>();
+    private Dictionary<int, ColourCondition> ColourList = new Dictionary<int, ColourCondition>();
+    private Dictionary<int, ItemCondition> ItemList = new Dictionary<int, ItemCondition>();
+    private Dictionary<int, PriceCondition> PriceList = new Dictionary<int, PriceCondition>();
     public int criteriaNumber = 1;
 
     public static event Action<int, int, int> OnCriteriaDecided;
-    private bool duplicateOccured = false;
+   // private bool duplicateOccured = false;
 
-    public enum RoundCondition
+   /* public enum RoundCondition
     {
         Fruit,
         Red,
@@ -41,7 +43,7 @@ public class CriteriaManager : MonoBehaviour
         NotSingle,
         NotOrange,
         NotDrink 
-    }
+    }*/
 
 
     public enum ColourCondition
@@ -68,7 +70,15 @@ public class CriteriaManager : MonoBehaviour
 
     public enum PriceCondition
     {
-        //TO BE ADDED
+        LessThan5Pounds,
+        MoreThan5Pounds,
+        LessThan3Pounds,
+        MoreThan3Pounds,
+        LessThan1Pound,
+        MoreThan1Pound,
+        LessThan2Pounds,
+        MoreThan2Pounds
+        //More to be added later once properly sorted out
     }
 
     /*public enum CriteriaOnScreen
@@ -80,43 +90,67 @@ public class CriteriaManager : MonoBehaviour
     }*/
 
     //populating dictionary
-    public void populateDict()
+    public void populateColourDict()
     {
-        CriteriaList.Clear();
+        ColourList.Clear();
 
-        CriteriaList.Add(1, RoundCondition.Fruit);
-        CriteriaList.Add(2, RoundCondition.Red);
-        CriteriaList.Add(3, RoundCondition.Green);
-        CriteriaList.Add(4, RoundCondition.Yellow);
-        CriteriaList.Add(5, RoundCondition.Single);
-        CriteriaList.Add(6, RoundCondition.Orange);
-        CriteriaList.Add(7, RoundCondition.Drink);
-        CriteriaList.Add(8, RoundCondition.NotFruit);
-        CriteriaList.Add(9, RoundCondition.NotRed);
-        CriteriaList.Add(10, RoundCondition.NotGreen);
-        CriteriaList.Add(11, RoundCondition.NotYellow);
-        CriteriaList.Add(12, RoundCondition.NotSingle);
-        CriteriaList.Add(13, RoundCondition.NotOrange);
-        CriteriaList.Add(14, RoundCondition.NotDrink);
+        ColourList.Add(1, ColourCondition.Red);
+        ColourList.Add(2, ColourCondition.Orange);
+        ColourList.Add(3, ColourCondition.Yellow);
+        ColourList.Add(4, ColourCondition.Green);
+        ColourList.Add(5, ColourCondition.NotRed);
+        ColourList.Add(6, ColourCondition.NotOrange);
+        ColourList.Add(7, ColourCondition.NotYellow);
+        ColourList.Add(8, ColourCondition.NotGreen);
+   
+    }
+
+    public void populateItemDict()
+    {
+        ItemList.Clear();
+
+        ItemList.Add(1, ItemCondition.Fruit);
+        ItemList.Add(2, ItemCondition.Drink);
+        ItemList.Add(3, ItemCondition.Single);
+        ItemList.Add(4, ItemCondition.NotFruit);
+        ItemList.Add(5, ItemCondition.NotDrink);
+        ItemList.Add(6, ItemCondition.NotSingle);
+    }
+
+    public void populatePriceDict()
+    {
+        PriceList.Clear();
+
+        PriceList.Add(1, PriceCondition.LessThan5Pounds);
+        PriceList.Add(2, PriceCondition.MoreThan5Pounds);
+        PriceList.Add(3, PriceCondition.LessThan3Pounds);
+        PriceList.Add(4, PriceCondition.MoreThan3Pounds);
+        PriceList.Add(5, PriceCondition.LessThan1Pound);
+        PriceList.Add(6, PriceCondition.MoreThan1Pound);
+        PriceList.Add(7, PriceCondition.LessThan2Pounds);
+        PriceList.Add(8, PriceCondition.MoreThan2Pounds);
+
+
+        //TO BE ADDED
     }
 
     public void IncreaseAmountOfCriteria() { criteriaNumber++; }
 
     public void selectCriteria()
     {
-        if (duplicateOccured != true)
-        {
+        /*if (duplicateOccured != true)
+       // {
             for (int i = 0; i < criteriaNumber;)
             {
                 int criteria;
-                criteria = Random.Range(1, CriteriaList.Count);
+                criteria = Random.Range(1, ColourList.Count);
                 //RoundCondition qRC = CriteriaList[key: amountOfCriteria];
                 //Debug.Log(qRC.ToString());
                 //_criteria1.text = "Criteria: " + qRC.ToString();
 
                 criteriaTextList[i] = criteria;
 
-                if(i != 0 && criteriaTextList[i] == criteriaTextList[i - 1])
+                /*if(i != 0 && criteriaTextList[i] == criteriaTextList[i - 1])
                 {
                     duplicateOccured = true;
                 }
@@ -129,13 +163,13 @@ public class CriteriaManager : MonoBehaviour
                     i++;
                 }
             }
-        }
+       // }
 
-        else
+       /* else
         {
             duplicateOccured = false;
             selectCriteria();
-        }
+        }*/
 
         //selectCriteria();
 
@@ -156,10 +190,36 @@ public class CriteriaManager : MonoBehaviour
          Debug.Log(qRC.ToString());
          _criteria1.text = "Criteria: " + qRC.ToString(); */
 
+        int colourKey;
+        int itemKey;
+        int priceKey;
+
+        colourKey = Random.Range(1, ColourList.Count);
+        itemKey = Random.Range(1, ItemList.Count);
+        priceKey = Random.Range(1, PriceList.Count);
+
+        if (criteriaNumber == 3)
+        {
+            criteriaTextList[0] = colourKey;
+            criteriaTextList[1] = itemKey;
+            criteriaTextList[2] = priceKey;
+        }
+
+        if (criteriaNumber == 2) 
+        {
+            criteriaTextList[0] = colourKey;
+            criteriaTextList[1] = itemKey;
+        }
+
+        if (criteriaNumber == 1)
+        {
+            criteriaTextList[0] = colourKey;
+        }
+
         if (criteriaTextList[0] != 0)
         {
-            _criteriaP1_1.text = "Criteria 1: " + CriteriaList[key: criteriaTextList[0]].ToString() + "\n";
-            _criteriaP2_1.text = "Criteria 1: " + CriteriaList[key: criteriaTextList[0]].ToString() + "\n";
+            _criteriaP1_1.text = "Criteria 1: " + ColourList[key: criteriaTextList[0]].ToString() + "\n";
+            _criteriaP2_1.text = "Criteria 1: " + ColourList[key: criteriaTextList[0]].ToString() + "\n";
         }
 
         //_criteriaP1_1.text = "Criteria 1: " + CriteriaList[key: criteriaTextList[0]].ToString() + "\n";
@@ -167,15 +227,15 @@ public class CriteriaManager : MonoBehaviour
 
         if (criteriaTextList[1] != 0)
         {
-            _criteriaP1_2.text = "Criteria 2: " + CriteriaList[key: criteriaTextList[1]].ToString() + "\n";
-            _criteriaP2_2.text = "Criteria 2: " + CriteriaList[key: criteriaTextList[1]].ToString() + "\n";
+            _criteriaP1_2.text = "Criteria 2: " + ItemList[key: criteriaTextList[1]].ToString() + "\n";
+            _criteriaP2_2.text = "Criteria 2: " + ItemList[key: criteriaTextList[1]].ToString() + "\n";
         }
             
 
         if (criteriaTextList[2] != 0)
         {
-            _criteriaP1_3.text = "Criteria 3: " + CriteriaList[key: criteriaTextList[2]].ToString() + "\n";
-            _criteriaP2_3.text = "Criteria 3: " + CriteriaList[key: criteriaTextList[2]].ToString() + "\n";
+            _criteriaP1_3.text = "Criteria 3: " + PriceList[key: criteriaTextList[2]].ToString() + "\n";
+            _criteriaP2_3.text = "Criteria 3: " + PriceList[key: criteriaTextList[2]].ToString() + "\n";
         }
            // _criteria3.text = "Criteria 3: " + CriteriaList[key: criteriaTextList[2]].ToString() + "\n";
 
@@ -190,7 +250,9 @@ public class CriteriaManager : MonoBehaviour
     {
         //for (int i = 0; i <= criteriaNumber; i++)
         //{
-            populateDict();
+            populateColourDict();
+            populateItemDict();
+            populatePriceDict();
             selectCriteria();
             //i++;
         //}
