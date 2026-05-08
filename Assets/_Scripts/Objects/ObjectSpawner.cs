5e47d9aaf7ae2013eb9304fde30c4078232d8da3
Nikolaos Comandariu <s283743@uos.ac.uk>
2026-05-08 11:47:59 +0100

    Feat (Playtest): Add particle effects and sfx for correct/wrong choice

--- Assets/_Scripts/Objects/ObjectSpawner.cs
+++ Assets/_Scripts/Objects/ObjectSpawner.cs
@@ -42,9 +42,10 @@ public class ObjectSpawner : MonoBehaviour
     [Header("Input Delay")]
     [SerializeField] private int InputDelayTime;
 
-    [Header("Particles")]
+    [Header("Game Objects")]
     [SerializeField] private GameObject correctParticles;
     [SerializeField] private GameObject wrongParticles;
+    [SerializeField] private AudioManager audioManager;
 
     [Header("Events")]
     public System.Action onAllObjectsProcessed; // Nikolaos Comandariu.
@@ -270,7 +271,7 @@ public class ObjectSpawner : MonoBehaviour
             GameObject instance = Instantiate(ScoreTextFeedback, position, Quaternion.identity);
 
             TextMeshPro tmp = instance.GetComponent<TextMeshPro>();
-            tmp.text = amount;
+            tmp.text = amount.ToString();
             tmp.color = color;
     }
 
@@ -413,17 +414,22 @@ public class ObjectSpawner : MonoBehaviour
                 {
                     score += 1;
                     DisplayTextFeedback(+1, CurrentObjLoc, Color.green);
+                    Instantiate(correctParticles, CurrentObjLoc, Quaternion.identity);
                 }
 
                 score += 1; // Score should not be in ObjectSpawner ideally, might need to refactor later.
                 DisplayTextFeedback(+1, CurrentObjLoc, Color.green);
+                Instantiate(correctParticles, CurrentObjLoc, Quaternion.identity);
                 UpdateScoreUI();
                 Debug.Log("Correct! Score is now: " + score);
+                audioManager.PlaySFX(audioManager.correctChoiceSFX);
             }
             else if(!isMatch)
             {
                 //Debug.Log("Wrong choice!");
                 score -= 1;
+                Instantiate(wrongParticles, CurrentObjLoc, Quaternion.identity);
+                audioManager.PlaySFX(audioManager.incorrectChoiceSFX);
                 //DisplayTextFeedback(-1, CurrentObjLoc, Color.red);
                 UpdateScoreUI();
                 NotMatch = true;
@@ -554,18 +560,23 @@ public class ObjectSpawner : MonoBehaviour
                 {
                     score += 1;
                     DisplayTextFeedback(+1, CurrentObjLoc, Color.green);
+                    Instantiate(correctParticles, CurrentObjLoc, Quaternion.identity);
                 }
 
                 score += 1; // Score should not be in ObjectSpawner ideally, might need to refactor later.
                 isMatch = true;
                 DisplayTextFeedback(+1, CurrentObjLoc, Color.green);
+                Instantiate(correctParticles, CurrentObjLoc, Quaternion.identity);
                 UpdateScoreUI();
                 Debug.Log("Correct! Score is now: " + score);
+                audioManager.PlaySFX(audioManager.correctChoiceSFX);
             }
             else if (!isMatch)
             {
                 //Debug.Log("Wrong choice!");
                 score -= 1;
+                Instantiate(wrongParticles, CurrentObjLoc, Quaternion.identity);
+                audioManager.PlaySFX(audioManager.incorrectChoiceSFX);
                 //DisplayTextFeedback(-1, CurrentObjLoc, Color.red);
                 UpdateScoreUI();
                 NotMatch = true;
