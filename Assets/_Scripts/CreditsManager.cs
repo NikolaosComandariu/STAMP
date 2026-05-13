d72c983bb11d1e4c4999674aff8ed76653ea874c
Nikolaos Comandariu <s283743@uos.ac.uk>
2026-05-13 11:59:14 +0100

    Feat (Credits): Create coroutine

--- Assets/_Scripts/CreditsManager.cs
+++ Assets/_Scripts/CreditsManager.cs
@@ -8,16 +8,37 @@ public class CreditsManager : MonoBehaviour
 
     [Header("Variables")]
     [SerializeField] private float stampTime;
+    [SerializeField] private float speed;
 
     private int amountOfNames = 7;
+    private bool keepMoving;
+
+    private void Start()
+    {
+        StartCoroutine(Credits());
+    }
+
+    private void Update()
+    {
+        if (!keepMoving) return;
+
+        creditNames[0].transform.position += speed * Time.deltaTime * Vector3.down;   
+    }
 
     private IEnumerator Credits()
     {
+        yield return new WaitForSeconds(stampTime);
+        keepMoving = false;
+
         for(int i = 0; i < amountOfNames; i++)
         {
+            keepMoving = false;
             creditNames[i].gameObject.GetComponent<SpriteRenderer>().enabled = true;
             yield return new WaitForSeconds(stampTime);
+            keepMoving = true;
+            yield return new WaitForSeconds(stampTime);
         }
-        
+
+        keepMoving = false;
     }
 }
