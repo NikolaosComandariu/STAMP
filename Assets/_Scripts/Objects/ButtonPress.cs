65d42c0fe4f6ac1820df2cf07e744f119a1fdb6b
JoshuaHuntJames <S298011@uos.ac.uk>
2026-05-20 10:28:18 +0100

    Button Press feedback working

---
+++ Assets/_Scripts/Objects/ButtonPress.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPress : MonoBehaviour
+{
+
+    [SerializeField] private Button Q;
+    [SerializeField] private Button E;
+    [SerializeField] private Button I;
+    [SerializeField] private Button P;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Q.image.color = Q.colors.pressedColor;
+        }
+        else
+        {
+            Q.image.color = Q.colors.normalColor;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            E.image.color = E.colors.pressedColor;
+        }
+        else
+        {
+            E.image.color = E.colors.normalColor;
+        }
+        if (Input.GetKey(KeyCode.I))
+        {
+            I.image.color = I.colors.pressedColor;
+        }
+        else
+        {
+            I.image.color = I.colors.normalColor;
+        }
+        if (Input.GetKey(KeyCode.P))
+        {
+            P.image.color = P.colors.pressedColor;
+        }
+        else
+        {
+            P.image.color = P.colors.normalColor;
+        }
+    }
+}
