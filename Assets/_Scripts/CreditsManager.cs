c9a6ec5972ace007ace576bb4b6a064e56807427
Nikolaos Comandariu <s283743@uos.ac.uk>
2026-05-20 11:51:47 +0100

    Save

--- Assets/_Scripts/CreditsManager.cs
+++ Assets/_Scripts/CreditsManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UIElements;
+using Unity.VisualScripting;
 
 public class CreditsManager : MonoBehaviour
 {
@@ -7,16 +9,21 @@ public class CreditsManager : MonoBehaviour
     [SerializeField] private GameObject[] creditNames;
     [SerializeField] private GameObject creditAnim;
 
+    [Header("Game Objects")]
+    [SerializeField] private Vector3[] creditsPos;
+
     [Header("Variables")]
     [SerializeField] private float stampTime;
     [SerializeField] private float speed;
     [SerializeField] private float initialWaitTime;
 
     private bool keepMoving;
+    private bool hasPlayedAlready = false;
+    private bool hasReset = true;
 
     private void Start()
     {
-        StartCoroutine(Credits());
+        //StartCoroutine(Credits());
     }
 
     private void Update()
@@ -33,10 +40,13 @@ public class CreditsManager : MonoBehaviour
 
     private IEnumerator Credits()
     {
+        hasReset = false;
+
         yield return new WaitForSeconds(initialWaitTime);
         keepMoving = false;
+        creditAnim.GetComponent<Animator>().SetBool("HasPlayed", false);
 
-        for(int i = 0; i < creditNames.Length; i++)
+        for (int i = 0; i < creditNames.Length; i++)
         {
             keepMoving = false;
             yield return new WaitForSeconds(stampTime);
@@ -48,11 +58,39 @@ public class CreditsManager : MonoBehaviour
             {
                 keepMoving = false; 
                 creditAnim.GetComponent<Animator>().SetTrigger("Finished");
+                creditAnim.GetComponent<Animator>().SetBool("HasPlayed", true);
             }
 
             yield return new WaitForSeconds(stampTime);
         }
 
         keepMoving = false;
+        hasPlayedAlready = true;  
+    }
+
+    public void StartCredits()
+    {
+        if (hasPlayedAlready || creditAnim.GetComponent<Animator>().GetBool("HasPlayed") == true) return;
+
+        /*if(hasReset)
+        {
+            for (int i = 0; i < creditNames.Length; i++)
+            {
+                creditsPos.SetValue(i, );
+            }
+        }*/
+
+        Debug.Log("Started credits");
+        StartCoroutine(Credits());
+    }
+
+    public void ResetCredits()
+    {
+        keepMoving = false;
+        hasPlayedAlready = false;
+
+        
+
+        StartCredits();
     }
 }
