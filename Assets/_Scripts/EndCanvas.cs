9ee7da01799442e4d01baf3a4601f6c550426d91
JoshuaHuntJames <S298011@uos.ac.uk>
2026-05-22 15:15:17 +0100

    Printer shows at end of game
    Stuff to be added . score shows on reciept

--- Assets/_Scripts/EndCanvas.cs
+++ Assets/_Scripts/EndCanvas.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System;
 
 public class EndCanvas : MonoBehaviour
 {
@@ -9,18 +10,48 @@ public class EndCanvas : MonoBehaviour
     [SerializeField] private TextMeshProUGUI summaryP2;
     [SerializeField] private TextMeshProUGUI WinnerDeclaration;
 
+    public static event Action onEndCanvasEnabled;
+
+
+
+
     // Scores.
     private int p1Score;
     private int p2Score;
 
+
     // Text.
     private string whoWon;
+
+    public Animator animator;
+
+
+    [SerializeField] private Transform P2ReceiptStartPos;
+    [SerializeField] private Transform P1ReceiptStartPos;
+    [SerializeField] private Transform P2ReceiptEndPos;
+    [SerializeField] private Transform P1ReceiptEndPos;
+    [SerializeField] private float ReceiptMoveSpeed;
+    [SerializeField] private GameObject P1Receipt; 
+    [SerializeField] private GameObject P2Receipt;
+    [SerializeField] private GameObject PrinterAnimation;
+
+
 
     private void Start()
     {
         p1Score = 0;
         p2Score = 0;
         gameObject.GetComponent<Canvas>().enabled = false;
+        PrinterAnimation.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T)) 
+        {
+            Debug.Log("KeyPressed - trying to move receipt");
+            P1Receipt.transform.position = Vector2.MoveTowards(P1ReceiptStartPos.position, P1ReceiptEndPos.position, ReceiptMoveSpeed * Time.deltaTime);
+        }
     }
 
     /// <summary>
@@ -28,8 +59,23 @@ public class EndCanvas : MonoBehaviour
     /// </summary>
     private void OnEnable()
     {
+        ButtonClick.onPrintReceiptP2 += PrintReceiptP2;
         GameManager.onGameOver += HandleGameOver;
+        ButtonClick.onPrintReceipt += PrintReceipt;
         ScoreManager.SendPlayerScores += SetScores;
+    }
+
+    private void PrintReceiptP2()
+    {
+        PrinterAnimation.SetActive(true);
+       
+    }
+
+    private void PrintReceipt()
+    {
+        
+        
+
     }
 
     /// <summary>
@@ -37,6 +83,8 @@ public class EndCanvas : MonoBehaviour
     /// </summary>
     private void OnDisable()
     {
+        ButtonClick.onPrintReceiptP2 -= PrintReceiptP2;
+        ButtonClick.onPrintReceipt -= PrintReceipt;
         GameManager.onGameOver -= HandleGameOver;
         ScoreManager.SendPlayerScores -= SetScores;
     }
@@ -49,6 +97,8 @@ public class EndCanvas : MonoBehaviour
         Time.timeScale = 0.0f;
         gameObject.GetComponent<Canvas>().enabled = true;
         CompareScores();
+        onEndCanvasEnabled?.Invoke();
+        PrinterAnimation.SetActive(true);
     }
 
     /// <summary>
--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -220,6 +220,7 @@ public class GameManager : MonoBehaviour
         objectSpawner.ResetObjects();
         rightObjSpawner.ResetObjects();
 
+
         onRoundEnded?.Invoke();
 
         StartCoroutine(NextRound());
